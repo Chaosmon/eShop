5d3b0ad900d50eb1167cf0046c7dd934d035ef42
DESKTOP-99EO64N\Chaosmon <nguyenhungcuchi@gmail.com>
2023-06-14 17:13:51 +0700

    Image manage_1

--- eShop.ViewModels/Catalog/Products/ProductCreateRequest.cs
+++ eShop.ViewModels/Catalog/Products/ProductCreateRequest.cs
@@ -7,8 +7,6 @@ namespace eShop.ViewModels.Catalog.Products
         public decimal Price { set; get; }
         public decimal OriginalPrice { set; get; }
         public int Stock { set; get; }
-        public int ViewCount { set; get; }
-        public DateTime DateCreated { set; get; }
         public string Name { set; get; }
         public string Description { set; get; }
         public string Details { set; get; }
